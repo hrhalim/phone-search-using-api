a80a1e40b900e5fe4bf7ac24b19f2e2b0644d790
hrhalim <hrhalimbd8624@gmail.com>
2023-03-14 01:52:35 +0600

    Upload project

---
+++ js/app.js
@@ -0,0 +1,122 @@
+const loadPhones = async(searchText, dataLimit) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}` 
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data, dataLimit);
+}
+
+const displayPhones = (phones, dataLimit) =>{
+    const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.innerHTML = "";
+    // phonesContainer.textContent = '';
+    // display 10 phones only 
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 6) {
+        phones = phones.slice(0, 6);
+        showAll.classList.remove('d-none');
+    }
+    else{
+        showAll.classList.add('d-none');
+    }
+     
+    // display no phones found
+    const noPhone = document.getElementById('no-found-message');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+    }
+    else{
+        noPhone.classList.add('d-none');
+    }
+    // display all phones
+    phones.forEach(phone =>{ 
+        const phoneWrapper = document.getElementById('phones-container'); 
+        const phoneDiv  = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card p-4">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
+                
+            </div>
+        </div>
+        `;
+        phoneWrapper.appendChild(phoneDiv);
+    });
+    // stop spinner or loader
+    toggleSpinner(false);
+}
+
+const processSearch = (dataLimit) =>{
+    // toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+}
+
+// handle search button click
+document.getElementById('btn-search').addEventListener('click', function(){
+    // start loader
+    
+    processSearch(6);
+})
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (e) { 
+    toggleSpinner(true);
+    if (e.key === 'Enter') { 
+        processSearch(6);
+    }
+    toggleSpinner(true);
+    
+});
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
+
+// not the best way to load show All
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    processSearch();
+})
+
+const loadPhoneDetails = async (id) =>{
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`;
+    console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone =>{
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.brand;
+    const phoneDetails = document.getElementById('phone-details'); 
+
+    const sensors = phone.mainFeatures.sensors;
+
+    let sensorsList = "";
+    for (const sensor of sensors) {
+        sensorsList += `<li class="custom-font-4">${sensor}</li>`;
+    };
+    phoneDetails.innerHTML = `
+        <p>Release Date: ${phone.releaseDate}</p>
+        <p>Storage: ${phone.mainFeatures.storage}</p>
+        <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}</p>
+        <p>Sensor:
+          <ol>${sensorsList}</ol>
+         </p>
+    `
+}
+
+loadPhones('oppo');
